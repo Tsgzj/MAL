16644c1b8bb12260dbe2a4683c6491e036293ee7
Tsgzj <swx@tsgzj.me>
2017-09-12 16:38:08 -0700

    Started step 0

---
+++ src/step_0.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char *_read() {
+  return NULL;
+}
+
+char *_eval() {
+  return NULL;
+}
+
+char *_print() {
+  return NULL;
+}
+
+inline char *_repl() {
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
+  repl();
+  return 0;
+}
