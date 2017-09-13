c61c3faf9d40443fc185f25cedabc0da133dd09b
Tsgzj <swx@tsgzj.me>
2017-09-12 17:57:56 -0700

    added clang-format file

--- src/step_0.c
+++ src/step_0.c
@@ -1,35 +1,34 @@
+#include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <readline/readline.h>
 
-extern inline void _repl ();
+extern inline void _repl();
 
 char *
-_read () {
-  char *line = readline ("user>");
+_read() {
+  char *line = readline("user>");
   return line;
 }
 
 char *
-_eval (char *eq) {
+_eval(char *eq) {
   return eq;
 }
 
 void
-_print (char *res) {
-  printf ("%s\n", res);
+_print(char *res) {
+  printf("%s\n", res);
 }
 
 void
-_repl () {
-  while (1)
-    {
-      _print (_eval (_read ()));
-    }
+_repl() {
+  while (1) {
+    _print(_eval(_read()));
+  }
 }
 
 int
-main (int argc, char *argv[]) {
-  _repl ();
+main(int argc, char *argv[]) {
+  _repl();
   return 0;
 }
